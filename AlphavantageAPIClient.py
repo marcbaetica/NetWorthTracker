627652286d09d0b61ab892a3451f7f80cd133358
Marc Baetica <marc.baetica@gmail.com>
2022-01-05 22:59:00 -0500

    Alphavantage api client implementation and env file.

---
+++ AlphavantageAPIClient.py
@@ -0,0 +1,33 @@
+import os
+import requests as req
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+from dotenv import load_dotenv
+from urllib.parse import urlencode
+
+load_dotenv()
+
+
+class AlphavantageAPIClient:
+    def __init__(self, stock):
+        self.base_url = 'https://www.alphavantage.co/query'
+        self.url_encoded_params = {
+                'function': 'GLOBAL_QUOTE',
+                'symbol': stock,
+                'apikey': os.getenv('ALPHAVANTAGE_KEY')
+        }
+
+    @property
+    def stock_price(self):
+        price = None
+        query_url = self.base_url + '?' + urlencode(self.url_encoded_params)
+        try:
+            res = req.get(query_url)
+            res.raise_for_status()
+            price = float(res.json()['Global Quote']['05. price'])
+        except ConnectionError:
+            print(f'Failed to connect to {query_url}.')
+        except Timeout:
+            print(f'Request to connect to {query_url} resulted in a timeout error.')
+        except TooManyRedirects:
+            print(f'The request to {query_url} exceeds the configured number of maximum redirections.')
+        return price
